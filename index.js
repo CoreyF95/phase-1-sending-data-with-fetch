43b01b53ade8c4e884f7d29f8109faa8f83d198e
Corey Fisher <coreyfisher2021@gmail.com>
2021-11-15 11:55:55 -0600

    commit tree dd396e1ced8c9d381efa86088fa9c90a3e91ad81

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+const body = document.getElementsByTagName("body")[0];
+
+function submitData(name, email){
+const configurationData = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    },
+    body: JSON.stringify({
+        "name": name,
+        "email": email,
+    }),
+}
+
+    return fetch("http://localhost:3000/users", configurationData)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            addNewContact(object.id);
+        })
+        .catch(function (error) {
+            alert("Unauthorized Access") 
+            document.body.textContent = error
+        })
+}
+
+function addNewContact(newId){
+    document.getElementsByTagName("body")[0].textContent = newId
+}
+
+submitData();
